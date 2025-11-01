460d1e5f132be0b7a41075918dbf166d0df64e6d
martinkab07 <martoo07@outlook.com>
2025-11-01 16:39:26 +0200

    Set up the backend (firebase)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
 }
 
 android {
@@ -21,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.volunteer_app"
+        applicationId = "com.volunteer.volunteer_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
