07071b4f50680904235d91ab5cbd944ba17b4aa0
Zvezdi <zvezdibaronz@gmail.com>
2025-11-08 09:46:42 +0200

    Reworked Registration page
    Now it has 3 fields and requires repetition of the password. Matched style with the updated sign-in page.

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -22,88 +22,179 @@ class _RegisterState extends State<Register> {
 
   String email = '';
   String password = '';
+  String repeatedPassword = '';
   String error = '';
+
+  bool _isPasswordVisible = false;
+  bool _isRepeatedPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.green[100],
-      appBar: AppBar(
-        backgroundColor: greenPrimary,
-        elevation: 0.0,
-        title: const Text('Регистрация'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Влезте'),
-            onPressed: () {
-              // Toggle to register view
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Имейл'),
-                validator: (val) => val!.isEmpty ? 'Моля въведете имейл' : null,
-                onChanged: (val) {
-                  // Handle email input change
-                  setState(() {
-                    email = val;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Парола'),
-                obscureText: true,
-                validator: (val) => val!.length < 6 ? 'Въведете парола с най-малко 6 знака' : null,
-                onChanged: (val) {
-                  // Handle password input change
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: accentAmber,
-                  foregroundColor: backgroundGrey,
-                ),
-                child: Text('Регистрация'),
-                onPressed: () async {
-                  // Handle sign-in button press
-                  if (_formKey.currentState!.validate()) {
-                    setState(() => loading = true);
-                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Моля въведете валиден имейл адрес';
-                        loading = false;
-                      });
-                    }
-                  }
-                },
-              ),
+      backgroundColor: backgroundGrey,
+      
+      // appBar: AppBar(
+      //   backgroundColor: greenPrimary,
+      //   elevation: 0.0,
+      //   title: const Text('Регистрация'),
+      //   centerTitle: true,
+      //   actions: <Widget>[
+      //     TextButton.icon(
+      //       icon: Icon(Icons.person),
+      //       label: Text('Влезте'),
+      //       onPressed: () {
+      //         // Toggle to register view
+      //         widget.toggleView();
+      //       },
+      //     )
+      //   ],
+      // ),
+      
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
 
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
+                SizedBox(height: 160.0),
+                const Text('Регистрация', style: mainHeadingStyle),
+                SizedBox(height: 30.0),
+
+                // Email input
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Имейл'),
+                  validator: (val) => val!.isEmpty ? 'Моля въведете имейл' : null,
+                  onChanged: (val) {
+                    // Handle email input change
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+
+                SizedBox(height: 20.0),
+
+                // Password input
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    hintText: 'Парола',
+                    suffixIcon: IconButton(
+                      // The icon changes depending on whether the password is visible or not
+                      icon: Icon(
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: greenPrimary
+                      ), 
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      })
+                    ),
+                  obscureText: !_isPasswordVisible,
+                  validator: (val) => val!.length < 6 ? 'Въведете парола с най-малко 6 знака' : null,
+                  onChanged: (val) {
+                    // Handle password input change
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+
+                SizedBox(height: 20.0),
+                
+                // Repeat password field
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    hintText: 'Повторете паролата',
+                    suffixIcon: IconButton(
+                      // The icon changes depending on whether the password is visible or not
+                      icon: Icon(
+                        _isRepeatedPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: greenPrimary
+                      ), 
+                      onPressed: () {
+                        setState(() {
+                          _isRepeatedPasswordVisible = !_isRepeatedPasswordVisible;
+                        });
+                      })
+                    ),
+                  obscureText: !_isRepeatedPasswordVisible,
+                  validator: (val) => val != password ? 'Паролите не съвпадат' : null,
+                  onChanged: (val) {
+                    // Handle password input change
+                    setState(() {
+                      repeatedPassword = val;
+                    });
+                  },
+                ),
+
+                SizedBox(height: 20.0),
+
+                // Registration button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: greenPrimary,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(double.infinity, 36),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text('Регистрирайте се!'),
+
+                  // Logs the user in if correct, throws error message otherwise
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() => loading = true);
+                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Настъпи грешка при регистрацията!';
+                          loading = false;
+                        });
+                      }
+                    }
+                  },
+                ),
+
+                SizedBox(height: 20.0),
+
+                // Switch to sign-in page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Имате регистрация?'),
+                    GestureDetector(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4.0, right: 4.0), 
+                      child: const Text(
+                        'Влезте!',
+                        style: TextStyle(
+                          color: greenPrimary, 
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      widget.toggleView();
+                    },
+                  ),
+                  ],
+                ),
+
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
 
             ],
           )
         ),
       ),
+    )
     );
   }
 }
