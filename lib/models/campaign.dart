3df5bbac57d4909d64c4548275f77ea914fd4fae
Zvezdi <zvezdibaronz@gmail.com>
2025-11-18 19:24:32 +0200

    Added Campaign and CampaignData class models

---
+++ lib/models/campaign.dart
@@ -0,0 +1,32 @@
+class Campaign {
+  final String id;
+  final String organizerId;
+  final String title;
+  final String description;
+  final String location;
+  final int requiredVolunteers;
+  final DateTime startDate;
+  final DateTime endDate;
+  final String imageUrl;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final List<String> categories;
+  final List<String> registeredVolunteersUids;
+
+  Campaign({
+    required this.id,
+    required this.organizerId, 
+    required this.title,
+    required this.description,
+    required this.location,
+    required this.requiredVolunteers,
+    required this.startDate,
+    required this.endDate,
+    required this.imageUrl, 
+    required this.createdAt,
+    required this.updatedAt,
+    required this.categories,
+    this.registeredVolunteersUids = const [],
+  });
+}
+  
---
+++ lib/models/campaign_data.dart
@@ -0,0 +1,10 @@
+class CampaignData {
+  String title = '';
+  String description = '';
+  String location = '';
+  int requiredVolunteers = 0;
+  DateTime? startDate;
+  DateTime? endDate;
+  String imageUrl = '';
+  List<String> categories = [];
+}
