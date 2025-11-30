aaea8a75acc5c59fa240682e940eb1909a70c26d
Zvezdi <zvezdibaronz@gmail.com>
2025-11-30 11:47:35 +0200

    Added unimported packages

--- lib/screens/main/profile.dart
+++ lib/screens/main/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:volunteer_app/models/volunteer.dart';
 import 'package:volunteer_app/shared/colors.dart';
 
 class ProfilePage extends StatefulWidget {
